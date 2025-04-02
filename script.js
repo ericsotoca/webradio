9015445099f8458189cbb5a9e0f59098918a7aa4
Eric Sotoca <151804279+ericsotoca@users.noreply.github.com>
2025-04-02 16:44:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,17 @@ const sunoTracks = [
     { "id": "423d3dea-7a36-4621-a3b2-ff92edc066d4", "title": "Bossa Jazz A Cappella", "artist": "3Daizy🌼", "ambiance": "Calme", "moment": "Après-midi", "intention": "Détente", "duration": 140 }
     { "id": "ffa48fbf-ac87-4a02-8cf2-f3766f518d58", "title": "I Spent 3000 Credits on This Song", "artist": "Nanashi_Zero🀄️", "ambiance": "Énergique", "moment": "Midi", "intention": "Motivation", "duration": 194 }
     { "id": "e48c0ee8-24c8-459b-8d91-469dbd6fc3a5", "title": "dialectic (accept it)", "artist": "yolkhead", "ambiance": "Calme", "moment": "Soir", "intention": "Détente", "duration": 239 }
+    { "id": "c5ebced9-f509-4d49-97b2-ff89ef881a52", "title": "Bob Ross Love Letter", "artist": "Suno AI", "ambiance": "Joyeuse", "moment": "Après-midi", "intention": "Détente", "duration": 180 }
+    { "id": "5564a0a7-ed9d-4b63-967b-c5534f472bce", "title": "π", "artist": "Suno AI", "ambiance": "Énergique", "moment": "Nuit", "intention": "Motivation", "duration": 180 }
+    { "id": "674e86cb-0395-414a-a291-a4c11a9efc4d", "title": "50 decimal places of pi", "artist": "Suno AI", "ambiance": "Calme", "moment": "Soir", "intention": "Réflexion", "duration": 180 }
+    { "id": "4ec14116-ac9b-49e3-ae62-99eb2f1ac0c2", "title": "Circles (Remastered)", "artist": "claraco", "ambiance": "Calme", "moment": "Soir", "intention": "Détente", "duration": 180 }
+    { "id": "2343eb4a-d3c6-40e7-bf40-91e76b72a25c", "title": "Escape from 9 to 5", "artist": "Adios", "ambiance": "Énergique", "moment": "Midi", "intention": "Motivation", "duration": 125 }
+    { "id": "93288786-8d6a-4e6a-95fd-4e3ba1aca02c", "title": "Why didn't anyone like my song?", "artist": "Suno AI", "ambiance": "Énergique", "moment": "Midi", "intention": "Motivation", "duration": 180 }
+    { "id": "063f3392-fe1b-4ce9-9ac8-660dd0315bfb", "title": "Mozart On The Bass- FI", "artist": "Suno AI", "ambiance": "Énergique", "moment": "Nuit", "intention": "Voyage", "duration": 180 }
+    { "id": "f5a86119-2283-4e9a-86ae-9c96476f1450", "title": "醉打蔣門神", "artist": "stratosphericpolyrhythm855", "ambiance": "Inconnu", "moment": "Inconnu", "intention": "Inconnu", "duration": 180 }
+    { "id": "8b6a2d3c-909f-4bee-8193-1f206b0cc111", "title": "OH AI (extended Version)", "artist": "SpinningFeedback620", "ambiance": "Mélancolique", "moment": "Soir", "intention": "Réflexion", "duration": 180 }
+    { "id": "1b28f52a-0311-48e1-a645-f06f6a6f3d66", "title": "Nikola Tesla", "artist": "crispity", "ambiance": "Énergique", "moment": "Midi", "intention": "Motivation", "duration": 214 }
+    { "id": "530b3957-d316-45d1-8bbf-989b1aa32a60", "title": "Sway Sway Sway Sway Sway Sway", "artist": "Shonen Beats", "ambiance": "Calme", "moment": "Soir", "intention": "Détente", "duration": 161 }
 ];
 
 // Variables globales
